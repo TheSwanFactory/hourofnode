c6cb2753e1823787ffbc3417e2ae3604acdf19a4
Ernest Prabhakar <info@theswanfactory.com>
2014-10-24 13:42:57 -0700

    aws test file

---
+++ aws/aws.js
@@ -0,0 +1,19 @@
+var AWS = require('aws-sdk'); 
+
+var s3 = new AWS.S3(); 
+
+ s3.createBucket({Bucket: 'myBucket'}, function() {
+
+  var params = {Bucket: 'myBucket', Key: 'myKey', Body: 'Hello!'};
+
+  s3.putObject(params, function(err, data) {
+
+      if (err)       
+
+          console.log(err)     
+
+      else       console.log("Successfully uploaded data to myBucket/myKey");   
+
+   });
+
+});
