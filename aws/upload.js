f79c4217b584920a45eb87aa1325d4cd061bcaf5
Ernest Prabhakar <info@theswanfactory.com>
2014-10-29 16:07:32 -0700

    Fix aws status message

--- aws/upload.js
+++ aws/upload.js
@@ -4,14 +4,15 @@ var fs = require('fs');
 AWS.config.region = 'us-west-1';
 var s3 = new AWS.S3();
 var bucket = 'hourofnode.org';
+var file = 'main.js'
 
 fs.readFile('web/main.js', function (err, data) {
-  var params = {Bucket: bucket, Key: 'main.js', Body: data};
+  var params = {Bucket: bucket, Key: file, Body: data};
   var result = s3.putObject(params, function(err, data) {
     if (err) {
       console.log("Error uploading data: ", err);
     } else {
-      console.log("Successfully uploaded data to myBucket/myKey");
+      console.log("Successfully uploaded " + file + " to " + bucket);
     }
   });
 });
